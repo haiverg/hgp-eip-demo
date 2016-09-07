806b4432faa1eb1555a34ea7e8d89c04afa8ace6
haiverg <haiver.galeano@gmail.com>
2016-09-06 22:15:46 -0500

    Initial checkin of eip demo lab made from scrash

---
+++ hgp-eip-demo/src/main/java/org/seaq/jb421/JavaRouteBuilder.java
@@ -0,0 +1,17 @@
+package org.seaq.jb421;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class JavaRouteBuilder extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("file:in?noop=true")
+		.choice()
+		.when(header("CamelFileNameOnly").endsWith(".txt")).to("file:txt")
+		.when(header("CamelFileNameOnly").endsWith(".xml")).to("file:xml")
+		.end();
+
+	}
+
+}
